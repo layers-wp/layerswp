c1c005cd17ac1dbc1c169244f09e9cae1b08257a
David Perel <david@obox.co.za>
2014-09-05 13:20:16 +0200

    Added text align to the font family

--- core/widgets/modules/framework.php
+++ core/widgets/modules/framework.php
@@ -76,21 +76,23 @@ if( !class_exists( 'Hatch_Framework_Widget' ) ) {
 					Display
 				</span>
 			</a>
-			<div class="hatch-design-properties">
-				<ul class="hatch-checkbox-list">
-					<li class="hatch-checkbox">
-						<input type="checkbox" id="widget-hatch-widget-map-3-hide_google_map" name="widget-hatch-widget-map[3][hide_google_map]"   />
-						<label for="widget-hatch-widget-map-3-hide_google_map">Hide Google Map</label>
-					</li>
-					<li class="hatch-checkbox">
-						<input type="checkbox" id="widget-hatch-widget-map-3-hide_address" name="widget-hatch-widget-map[3][hide_address]"   />
-						<label for="widget-hatch-widget-map-3-hide_address">Hide Address</label>
-					</li>
-					<li class="hatch-checkbox">
-						<input type="checkbox" id="widget-hatch-widget-map-3-hide_contact_form" name="widget-hatch-widget-map[3][hide_contact_form]"   />
-						<label for="widget-hatch-widget-map-3-hide_contact_form">Hide Contact Form</label>
-					</li>
-				</ul>
+			<div class="hatch-design-properties-wrapper">
+				<div class="hatch-design-properties">
+					<ul class="hatch-checkbox-list">
+						<li class="hatch-checkbox">
+							<input type="checkbox" id="widget-hatch-widget-map-3-hide_google_map" name="widget-hatch-widget-map[3][hide_google_map]"   />
+							<label for="widget-hatch-widget-map-3-hide_google_map">Hide Google Map</label>
+						</li>
+						<li class="hatch-checkbox">
+							<input type="checkbox" id="widget-hatch-widget-map-3-hide_address" name="widget-hatch-widget-map[3][hide_address]"   />
+							<label for="widget-hatch-widget-map-3-hide_address">Hide Address</label>
+						</li>
+						<li class="hatch-checkbox">
+							<input type="checkbox" id="widget-hatch-widget-map-3-hide_contact_form" name="widget-hatch-widget-map[3][hide_contact_form]"   />
+							<label for="widget-hatch-widget-map-3-hide_contact_form">Hide Contact Form</label>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</li>
 
@@ -110,25 +112,27 @@ if( !class_exists( 'Hatch_Framework_Widget' ) ) {
 					List Style
 				</span>
 			</a>
-			<div class="hatch-design-properties hatch-small">
-				<a href="" class="hatch-design-control">
-					<span class="icon-list-grid"></span>
-					<span class="hatch-control-description">
-						Grid
-					</span>
-				</a>
-				<a href="" class="hatch-design-control">
-					<span class="icon-list-masonry"></span>
-					<span class="hatch-control-description">
-						Masonry
-					</span>
-				</a>
-				<a href="" class="hatch-design-control">
-					<span class="icon-list-list"></span>
-					<span class="hatch-control-description">
-						List
-					</span>
-				</a>
+			<div class="hatch-design-properties-wrapper hatch-small">
+				<div class="hatch-design-properties">
+					<a href="" class="hatch-design-control">
+						<span class="icon-list-grid"></span>
+						<span class="hatch-control-description">
+							Grid
+						</span>
+					</a>
+					<a href="" class="hatch-design-control">
+						<span class="icon-list-masonry"></span>
+						<span class="hatch-control-description">
+							Masonry
+						</span>
+					</a>
+					<a href="" class="hatch-design-control">
+						<span class="icon-list-list"></span>
+						<span class="hatch-control-description">
+							List
+						</span>
+					</a>
+				</div>
 			</div>
 		</li>
 
@@ -166,25 +170,27 @@ if( !class_exists( 'Hatch_Framework_Widget' ) ) {
 					Image Align
 				</span>
 			</a>
-			<div class="hatch-design-properties hatch-small">
-				<a href="" class="hatch-design-control">
-					<span class="icon-image-left"></span>
-					<span class="hatch-control-description">
-						Left
-					</span>
-				</a>
-				<a href="" class="hatch-design-control">
-					<span class="icon-image-right"></span>
-					<span class="hatch-control-description">
-						Right
-					</span>
-				</a>
-				<a href="" class="hatch-design-control">
-					<span class="icon-image-center"></span>
-					<span class="hatch-control-description">
-						Center
-					</span>
-				</a>
+			<div class="hatch-design-properties-wrapper hatch-small">
+				<div class="hatch-design-properties">
+					<a href="" class="hatch-design-control">
+						<span class="icon-image-left"></span>
+						<span class="hatch-control-description">
+							Left
+						</span>
+					</a>
+					<a href="" class="hatch-design-control">
+						<span class="icon-image-right"></span>
+						<span class="hatch-control-description">
+							Right
+						</span>
+					</a>
+					<a href="" class="hatch-design-control">
+						<span class="icon-image-center"></span>
+						<span class="hatch-control-description">
+							Center
+						</span>
+					</a>
+				</div>
 			</div>
 		</li>
 		<li class="hatch-quick-links">

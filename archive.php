364bc9fc0a3cac51b24ef660b0b830096f1fe8ec
Marc Perel <marc@obox.co.za>
2014-09-05 11:30:20 +0200

    Added sidebar to archive.php

--- archive.php
+++ archive.php
@@ -17,18 +17,22 @@
 			<?php while( have_posts() ) : the_post(); ?>
 				<article class="row">
 					<header class="section-title">
-						<h1 class="heading"><?php the_title(); ?></h1>
+						<h1 class="heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 						<h5 class="the-date"><?php the_date(); ?></h5>
 					</header>
-					<?php if( has_post_thumbnail() ) { ?>
-						<div class="column span-4 thumbnail-media">
-							<?php echo the_post_thumbnail( 'medium' ); ?>
-						</div>
-					<?php } // if has_post_thumbnail() ?>
-					<div class="column span-8">
-						<div class="copy">
-							<?php the_excerpt(); ?>
-							<a href="<?php the_permalink(); ?>" class="button"><?php _e( 'Read Post &rarr;' , HATCH_THEME_SLUG ); ?></a>
+					<div class="row">
+						<?php $content_class = 'span-12'; ?>
+						<?php if( has_post_thumbnail() ) { ?>
+							<div class="column span-4 thumbnail-media">
+								<?php echo the_post_thumbnail( 'medium' ); ?>
+							</div>
+							<?php $content_class = 'span-8'; ?>
+						<?php } // if has_post_thumbnail() ?>
+						<div class="column <?php echo $content_class; ?>">
+							<div class="copy">
+								<?php the_excerpt(); ?>
+								<a href="<?php the_permalink(); ?>" class="button"><?php _e( 'Read Post &rarr;' , HATCH_THEME_SLUG ); ?></a>
+							</div>
 						</div>
 					</div>
 				</article>

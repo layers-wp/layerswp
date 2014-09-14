f93cf3b34e1b86171d320a1ae9eafb85ec602e28
Marc Perel <marc@obox.co.za>
2014-09-14 23:30:24 +0200

    Personalized welcome message

--- core/options-panel/partials/welcome.php
+++ core/options-panel/partials/welcome.php
@@ -1,10 +1,9 @@
-
-
+<?php $user = wp_get_current_user(); ?>
 <section class="hatch-container hatch-content-large">
-	
+
 	<div class="hatch-section-title hatch-large invert hatch-content-massive" style="background: url(<?php echo get_template_directory_uri(); ?>/images/beta-zero.jpg) top repeat;">
 		<div class="hatch-container">
-			<h2 class="hatch-heading">Welcome {name}, thank you for beta testing Hatch!</h2>
+			<h2 class="hatch-heading">Welcome <?php echo ( ( '' == $user->user_firstname ) ? $user->display_name : $user->user_firstname ); ?>, thank you for beta testing Hatch!</h2>
 			<p class="hatch-excerpt">
 				Hatch is a powerful site builder that gives you the tools to create not only the site you want but the site you need.
 				The aim of Hatch is to turn the task of making a website into a joyful and easy experience.

53a80196180dc237aed902ee64260e747a6e126e
Marc Perel <marc@Marcs-MacBook-Pro.local>
2014-10-01 18:30:42 +0200

    Added breadcrumbs Added pagination Added comments Added new post list HTML Added post page HTML Added portfolio page HMTL Added page title partial

--- archive.php
+++ archive.php
@@ -1,24 +1,15 @@
 <?php get_header(); ?>
-<div class="title-container">
-	<div class="title">
-		<h3 class="header"><?php echo single_cat_title( '', false ); ?></h3>
-		<?php
-		// Show an optional term description.
-			$term_description = term_description();
-			if ( ! empty( $term_description ) ) :
-				printf( '<p class="excerpt">%s</p>', $term_description );
-			endif;
-		?>
-	</div>
-</div>
+<?php get_template_part( 'partials/header' , 'page-title' ); ?>
 <section <?php post_class( 'container content-main archive clearfix' ); ?>>
 	<?php if( have_posts() ) : ?>
 		<div class="column span-7">
 			<?php while( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content' , 'list' ); ?>
 			<?php endwhile; // while has_post(); ?>
+			<?php hatch_pagination(); ?>
 		</div>
 	<?php endif; // if has_post() ?>
+
 	<div class="column span-4 pull-right well content">
 		<?php get_sidebar(); ?>
 	</div>
---
+++ partials/header-page-title.php
@@ -0,0 +1,14 @@
+<?php $details = hatch_get_page_title(); ?>
+<div class="title-container">
+    <div class="title">
+
+            <?php hatch_bread_crumbs(); ?>
+
+            <?php if( isset( $details[ 'title' ] ) ) { ?>
+                <h3 class="heading"><?php echo $details[ 'title' ]; ?></h3>
+            <?php } // if isset $title ?>
+            <?php if( isset( $details[ 'excerpt' ] ) ) { ?>
+                <p class="excerpt"><?php echo $details[ 'excerpt' ]; ?></p>
+            <?php } // if isset $excerpt ?>
+        </div>
+</div>

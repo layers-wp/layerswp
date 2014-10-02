ded1df48b40a18ae2b4cc844b28864dd8c95447b
Marc Perel <marc@Marcs-MacBook-Pro.local>
2014-10-03 00:15:19 +0200

    Made each portfolio lsit item an include

--- portfolio.php
+++ portfolio.php
@@ -22,24 +22,7 @@ get_header(); ?>
             <?php while( $portfolio_query->have_posts() ) {
                 $portfolio_query->the_post();
                 global $post; ?>
-                <article class="column span-4 thumbnail">
-                    <?php if( has_post_thumbnail() ) { ?>
-                        <div class="thumbnail-media">
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_post_thumbnail( 'square-large' );  ?>
-                            </a>
-                        </div>
-                    <?php } // if post thumbnail ?>
-                    <div class="thumbnail-body">
-                        <div class="overlay">
-                            <header class="article-title">
-                                <h4 class="heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                            </header>
-                            <?php echo '<p class="excerpt">' . get_the_excerpt() . '</p>'; ?>
-                            <a href="<?php the_permalink(); ?>" class="button"><?php _e( 'View Project' , HATCH_THEME_SLUG ); ?></a>
-                        </div>
-                    </div>
-                </article>
+                <?php get_template_part( 'portfolio-list' ); ?>
             <?php }; // while have_posts ?>
         </div>
         <div class="row">
--- taxonomy-jetpack-portfolio-type.php
+++ taxonomy-jetpack-portfolio-type.php
@@ -17,25 +17,8 @@ get_header(); ?>
         <div class="row">
             <?php while( $wp_query->have_posts() ) {
                 $wp_query->the_post();
-                global $post; ?>
-                <article class="column span-4 thumbnail">
-                    <?php if( has_post_thumbnail() ) { ?>
-                        <div class="thumbnail-media">
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_post_thumbnail( 'square-large' );  ?>
-                            </a>
-                        </div>
-                    <?php } // if post thumbnail ?>
-                    <div class="thumbnail-body">
-                        <div class="overlay">
-                            <header class="article-title">
-                                <h4 class="heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                            </header>
-                            <?php echo '<p class="excerpt">' . get_the_excerpt() . '</p>'; ?>
-                            <a href="<?php the_permalink(); ?>" class="button"><?php _e( 'View Project' , HATCH_THEME_SLUG ); ?></a>
-                        </div>
-                    </div>
-                </article>
+                global $post;  ?>
+                <?php get_template_part( 'portfolio-list' ); ?>
             <?php }; // while have_posts ?>
         </div>
         <div class="row">

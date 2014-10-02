2b5eaddb5dbeb6691e330d51971ad4dfeac6d017
Marc Perel <marc@Marcs-MacBook-Pro.local>
2014-10-03 00:09:56 +0200

    Linked site title

--- partials/header-logo.php
+++ partials/header-logo.php
@@ -9,7 +9,7 @@
     if( function_exists( 'the_site_logo' ) ) the_site_logo(); ?>
     <?php if('blank' != get_theme_mod('header_textcolor') ) { ?>
         <div class="site-description">
-            <h3 class="sitename"><a href="<?php home_url(); ?>"><?php echo get_bloginfo( 'title' ); ?></a></h3>
+            <h3 class="sitename"><a href="<?php echo home_url(); ?>"><?php echo get_bloginfo( 'title' ); ?></a></h3>
             <p class="tagline"><?php echo get_bloginfo( 'description' ); ?></p>
         </div>
     <?php } ?>

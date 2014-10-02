82b09c29d330bd77d703a8d0bb778a64cf719883
Marc Perel <marc@Marcs-MacBook-Pro.local>
2014-10-02 15:05:35 +0200

    Added comments section

--- core/helpers/post.php
+++ core/helpers/post.php
@@ -98,3 +98,35 @@ if ( ! function_exists( 'hatch_get_the_author' ) ) :
         );
     }
 endif;
+
+function hatch_comment($comment, $args, $depth) {
+    $GLOBALS['comment'] = $comment;?>
+    <?php if( 1 < $depth && isset( $GLOBALS['lastdepth'] ) && $depth != $GLOBALS['lastdepth'] ) { ?>
+        <div class="row comments-nested push-top">
+    <?php } ?>
+    <div <?php comment_class( 'content push-bottom well' ); ?> id="comment-<?php comment_ID(); ?>">
+        <div class="avatar push-bottom clearfix">
+            <?php edit_comment_link(__('(Edit)', HATCH_THEME_SLUG),'<small class="pull-right">','</small>') ?>
+            <a class="avatar-image" href="">
+                <?php echo get_avatar($comment, $size = '70'); ?>
+            </a>
+            <div class="avatar-body">
+                <h5><?php echo get_comment_author_link(); ?></h5>
+                <small><?php printf(__('%1$s at %2$s', HATCH_THEME_SLUG), get_comment_date(),  get_comment_time()) ?></small>
+            </div>
+        </div>
+
+        <div class="copy small">
+            <?php if ($comment->comment_approved == '0') : ?>
+                <em><?php _e('Your comment is awaiting moderation.', HATCH_THEME_SLUG) ?></em>
+                <br />
+            <?php endif; ?>
+            <?php comment_text() ?>
+            <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+        </div>
+    <?php if( 1 < $depth && isset( $GLOBALS['lastdepth'] ) && $depth == $GLOBALS['lastdepth'] ) { ?>
+        </div>
+    <?php } ?>
+
+    <?php $GLOBALS['lastdepth'] = $depth; ?>
+<?php }

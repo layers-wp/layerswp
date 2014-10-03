024313577c6f6de52e21189989167edf6b326d93
Marc Perel <marc@Marcs-MacBook-Pro.local>
2014-10-03 02:05:22 +0200

    Added Google fonts

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 				echo ' | ' . sprintf( __( 'Page %s', HATCH_THEME_SLUG ), max( $paged, $page ) );
 		?>
 	</title>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<link rel="icon" href="images/temp-ts-fav.png" type="image/png" />
 	<?php wp_head(); ?>
 </head>

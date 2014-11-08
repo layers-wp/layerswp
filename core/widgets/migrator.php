95a9ea49f09fa9890da212b44f293d4b3e1836c4
Marc Perel <marc@Marcs-MacBook-Pro.local>
2014-11-08 21:33:18 +0200

    Added burger menu

---
+++ core/widgets/migrator.php
@@ -0,0 +1,49 @@
+<?php /**
+ * Widget Exporter
+ *
+ * This file contains the widget export/Import functionality in Hatch
+ *
+ * @package Hatch
+ * @since Hatch 1.0
+ */
+
+class Hatch_Widget_Migrator {
+
+    private static $instance;
+
+    /**
+    *  Initiator
+    */
+
+    public static function init(){
+        return self::$instance;
+    }
+
+    /**
+    *  Constructor
+    */
+
+    public function __construct() {
+    }
+
+    /**
+    *  Export
+    */
+
+    public function export() {
+    }
+
+    /**
+    *  Import
+    */
+
+    public function import() {
+    }
+
+    /**
+    *  Validate Input (Look for images)
+    */
+
+    public function validate_input() {
+    }
+}
--- partials/header-standard.php
+++ partials/header-standard.php
@@ -1,5 +1,12 @@
 <?php get_template_part( 'partials/header' , 'logo' ); ?>
 
-<?php do_action( 'hatch_before_header_nav' ); ?>
-<?php wp_nav_menu( array( 'theme_location' => HATCH_THEME_SLUG . '-primary' ,'container' => 'nav', 'container_class' => 'nav nav-horizontal', 'fallback_cb' => false )); ?>
-<?php do_action( 'hatch_after_header_nav' );
+<nav class="nav nav-horizontal">
+    <?php do_action( 'hatch_before_header_nav' ); ?>
+    <?php wp_nav_menu( array( 'theme_location' => HATCH_THEME_SLUG . '-primary' ,'container' => FALSE, 'fallback_cb' => false )); ?>
+    <a href="" class="responsive-nav">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </a>
+    <?php do_action( 'hatch_after_header_nav' ); ?>
+</nav>

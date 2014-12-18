b88f74a4ef6d0eb2d6cfd6d971fa280a7aceff4f
Marc Perel <marc@obox.co.za>
2014-12-18 11:11:00 +0200

    No widget setting

--- footer.php
+++ footer.php
@@ -7,19 +7,25 @@
 			<?php do_action( 'hatch_before_footer_inner' ); ?>
 			<div class="<?php if( 'layout-fullwidth' != hatch_get_theme_mod( 'footer-layout-width' ) ) echo 'container'; ?>  content-main clearfix">
 
-				<div class="row">
-					<?php do_action( 'hatch_before_footer_sidebar' ); ?>
-					<?php // Do logic related to the footer widget area count
-					$footer_sidebar_count = hatch_get_theme_mod( 'footer-layout-widget-area-count' );
-					if( '' == $footer_sidebar_count ) $footer_sidebar_count = 4;
-					$footer_sidebar_class = floor( 12/$footer_sidebar_count ); ?>
-					<?php for( $footer = 1; $footer <= $footer_sidebar_count; $footer++ ) { ?>
-						<div class="column span-<?php echo $footer_sidebar_class; ?> <?php if( $footer == $footer_sidebar_count ) echo 'last'; ?>">
-							<?php dynamic_sidebar( HATCH_THEME_SLUG . '-footer-' . $footer ); ?>
-						</div>
-					<?php } ?>
-					<?php do_action( 'hatch_after_footer_sidebar' ); ?>
-				</div>
+				<?php // Do logic related to the footer widget area count
+				$footer_sidebar_count = hatch_get_theme_mod( 'footer-layout-widget-area-count' ); ?>
+
+				<?php if( 0 != $footer_sidebar_count ) { ?>
+					<div class="row">
+						<?php do_action( 'hatch_before_footer_sidebar' ); ?>
+						<?php // Default Sidebar count to 4
+						if( '' == $footer_sidebar_count ) $footer_sidebar_count = 4;
+
+						// Get the sidebar class
+						$footer_sidebar_class = floor( 12/$footer_sidebar_count ); ?>
+						<?php for( $footer = 1; $footer <= $footer_sidebar_count; $footer++ ) { ?>
+							<div class="column span-<?php echo $footer_sidebar_class; ?> <?php if( $footer == $footer_sidebar_count ) echo 'last'; ?>">
+								<?php dynamic_sidebar( HATCH_THEME_SLUG . '-footer-' . $footer ); ?>
+							</div>
+						<?php } ?>
+						<?php do_action( 'hatch_after_footer_sidebar' ); ?>
+					</div>
+				<?php } // if 0 != sidebars ?>
 
 				<?php do_action( 'hatch_before_footer_copyright' ); ?>
 				<div class="row copyright">

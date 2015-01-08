87fe8bd403b253082c246d8873d459e7fad803e0
Marc Perel <marc@obox.co.za>
2015-01-08 13:42:52 +0200

    Footer layer edit

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 		<?php do_action( 'layers_before_footer' ); ?>
 		<footer id="footer" class="footer-site well">
 			<?php do_action( 'layers_before_footer_inner' ); ?>
-			<div class="<?php if( 'layout-fullwidth' != layers_get_theme_mod( 'footer-layout-width' ) ) echo 'container'; ?>  content-vertical-large clearfix">
+			<div class="<?php if( 'layout-fullwidth' != layers_get_theme_mod( 'footer-layout-width' ) ) echo 'container'; ?>  content-vertical-large content clearfix">
 
 				<?php // Do logic related to the footer widget area count
 				$footer_sidebar_count = layers_get_theme_mod( 'footer-layout-widget-area-count' ); ?>

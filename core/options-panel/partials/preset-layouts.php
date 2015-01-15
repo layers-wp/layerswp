100c2d9c4ab2d325fc2a3a84ae25e8590bd30d0e
David Perel <david@obox.co.za>
2015-01-15 10:58:20 +0200

    Layout change to presets page

--- core/options-panel/partials/preset-layouts.php
+++ core/options-panel/partials/preset-layouts.php
@@ -26,6 +26,14 @@ $find_builder_page = layers_get_builder_pages(); ?>
    <div class="layers-row layers-well layers-content-massive">
       <div class="layers-browser">
          <div class="layers-products">
+            <div class="layers-product blank-product" id="layers-generate-preset-layout-blank" data-key="layers-preset-layout-blank">
+               <input id="layers-preset-layout-blank-title" type="hidden" value="<?php _e( 'Blank' , LAYERS_THEME_SLUG ); ?>" />
+               <input id="layers-preset-layout-blank-widget_data" type="hidden" value="<?php json_encode( array() ); ?>" />
+               <div class="layers-product-screenshot">
+                  <span></span>
+               </div>
+               <h3 class="layers-product-name"><?php _e( 'Blank Canvas' , LAYERS_THEME_SLUG ); ?></h3>
+            </div>
             <?php foreach( $layers_migrator->get_preset_layouts() as $template_key => $template ) { ?>
                <div class="layers-product active  <?php echo ( isset( $template[ 'container_css' ] ) ?  $template[ 'container_css' ] : '' ); ?>" tabindex="0">
 
@@ -42,14 +50,6 @@ $find_builder_page = layers_get_builder_pages(); ?>
                   </div>
                </div>
             <?php } // Get Preset Layouts ?>
-            <div class="layers-product blank-product" id="layers-generate-preset-layout-blank" data-key="layers-preset-layout-blank">
-               <input id="layers-preset-layout-blank-title" type="hidden" value="<?php _e( 'Blank' , LAYERS_THEME_SLUG ); ?>" />
-               <input id="layers-preset-layout-blank-widget_data" type="hidden" value="<?php json_encode( array() ); ?>" />
-               <div class="layers-product-screenshot">
-                  <span></span>
-               </div>
-               <h3 class="layers-product-name"><?php _e( 'Blank Canvas' , LAYERS_THEME_SLUG ); ?></h3>
-            </div>
          </div>
       </div>
    </div>

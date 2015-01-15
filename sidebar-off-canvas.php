ec4e11eff0f8d871ee1a55c3547919738ff62406
Marc Perel <marc@obox.co.za>
2015-01-15 17:15:40 +0200

    Sidebar nav added .nav-vertical

--- sidebar-off-canvas.php
+++ sidebar-off-canvas.php
@@ -1,7 +1,7 @@
 <section class="wrapper darken invert off-canvas-right" id="off-canvas-right">
     <a class="close-canvas l-close" data-toggle="#off-canvas-right" data-toggle-class="open"></a>
     <div class="row push-top-large">
-        <?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-primary' ,'container' => 'nav', 'container_class' => 'nav nav-horizontal', 'fallback_cb' => create_function('', 'echo "&nbsp";') ) ); ?>
+        <?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-primary' ,'container' => 'nav', 'container_class' => 'nav nav-vertical', 'fallback_cb' => create_function('', 'echo "&nbsp";') ) ); ?>
     </div>
     <?php dynamic_sidebar( LAYERS_THEME_SLUG . '-off-canvas-sidebar' ); ?>
 </section>

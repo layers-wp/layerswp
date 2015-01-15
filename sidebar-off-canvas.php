61df62475844db80fc9b743efd3c4fa64ac110a3
Marc Perel <marc@obox.co.za>
2015-01-15 17:01:20 +0200

    Added primary nav to the off site sidebar

--- sidebar-off-canvas.php
+++ sidebar-off-canvas.php
@@ -1,4 +1,5 @@
 <section class="wrapper darken invert off-canvas-right" id="off-canvas-right">
     <a class="close-canvas l-close" data-toggle="#off-canvas-right" data-toggle-class="open"></a>
+    <?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-primary' ,'container' => 'nav', 'container_class' => 'nav nav-horizontal', 'fallback_cb' => create_function('', 'echo "&nbsp";') ) ); ?>
     <?php dynamic_sidebar( LAYERS_THEME_SLUG . '-off-canvas-sidebar' ); ?>
 </section>

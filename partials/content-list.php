0ab0f4e79ab35e01b473bf12956e3719182ccb59
Marc Perel <marc@obox.co.za>
2015-01-28 11:41:37 +0200

    get_the_ID();

--- partials/content-list.php
+++ partials/content-list.php
@@ -12,12 +12,12 @@ global $post, $layers_post_meta_to_display; ?>
         <h1 class="heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     </header>
     <?php // Layers Featured Media
-    echo layers_post_featured_media( array( 'postid' => $post->ID, 'wrap_class' => 'thumbnail push-bottom', 'size' => 'large' ) ); ?>
+    echo layers_post_featured_media( array( 'postid' => get_the_ID(), 'wrap_class' => 'thumbnail push-bottom', 'size' => 'large' ) ); ?>
     <?php if( '' != get_the_excerpt() || '' != get_the_content() ) { ?>
         <div class="copy">
             <?php the_excerpt(); ?>
         </div>
     <?php } ?>
-    <?php layers_post_meta( $post->ID, NULL, 'footer', 'meta-info push-bottom' ); ?>
+    <?php layers_post_meta( get_the_ID(), NULL, 'footer', 'meta-info push-bottom' ); ?>
     <p><a href="<?php the_permalink(); ?>" class="button"><?php _e( 'Read More' , LAYERS_THEME_SLUG ); ?></a></p>
 </article>

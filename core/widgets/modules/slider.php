63484739852872cca55c44ace21101be4048cfce
Marc Perel <marc@obox.co.za>
2015-01-28 15:51:36 +0200

    Removed inline styling from the slider.

--- core/widgets/modules/slider.php
+++ core/widgets/modules/slider.php
@@ -142,8 +142,7 @@ if( !class_exists( 'Layers_Slider_Widget' ) ) {
 								<?php if( false != $this->check_and_return( $slide , 'image' ) || 'image-left' == $slide['design'][ 'imagealign' ] || 'image-top' == $slide['design'][ 'imagealign' ] ) echo 'has-image'; ?>
 								<?php if( isset( $slide['design'][ 'imagealign' ] ) && '' != $slide['design'][ 'imagealign' ] ) echo $slide['design'][ 'imagealign' ]; ?>
 								<?php if( isset( $slide['design']['fonts'][ 'align' ] ) && '' != $slide['design']['fonts'][ 'align' ] ) echo $slide['design']['fonts'][ 'align' ]; ?>
-								"
-								style="float: left;">
+								">
 								<div class="overlay <?php if( isset( $slide['design'][ 'background' ][ 'darken' ] ) ) echo 'darken'; ?>"  <?php if( $this->check_and_return( $widget , 'slide_height' ) ) echo 'style="height: ' . $widget['slide_height'] . 'px;"' ?>>
 									<div class="container clearfix">
 										<?php if( '' != $slide['title'] || '' != $slide['excerpt'] || '' != $slide['link'] ) { ?>

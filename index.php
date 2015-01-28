b963581a3075fbaab2df04a7796f3faaba8238b9
Marc Perel <marc@obox.co.za>
2015-01-28 12:18:15 +0200

    Moved to get_sidebar(); in index.php

--- index.php
+++ index.php
@@ -9,25 +9,17 @@
 get_header(); ?>
 
 <section <?php post_class( 'content-main archive clearfix' ); ?>>
-
-    <?php /**
-    * Maybe show the left sidebar
-    */
-    layers_maybe_get_sidebar( 'left-sidebar', 'column pull-left sidebar' ); ?>
+    <?php get_sidebar( 'left' ); ?>
 
     <?php if( have_posts() ) : ?>
         <div <?php layers_center_column_class(); ?>>
             <?php while( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'partials/content' , 'list' ); ?>
             <?php endwhile; // while has_post(); ?>
-
             <?php the_posts_pagination(); ?>
         </div>
     <?php endif; // if has_post() ?>
 
-    <?php /**
-    * Maybe show the right sidebar
-    */
-    layers_maybe_get_sidebar( 'right-sidebar', 'column pull-right sidebar' ); ?>
+    <?php get_sidebar( 'right' ); ?>
 </section>
 <?php get_footer();

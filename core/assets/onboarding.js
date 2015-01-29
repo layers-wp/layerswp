ac7d6996038fe38019eed94f0b7bd83babd52e40
Marc Perel <marc@obox.co.za>
2015-01-29 16:48:52 +0200

    Onboarding next and skip buttons

--- core/assets/onboarding.js
+++ core/assets/onboarding.js
@@ -18,17 +18,53 @@
 
 jQuery(function($) {
 
+    $(document).on( 'click' , '.layers-onboard-slide .layers-button-well a.btn-primary' , function(e){
+        e.preventDefault();
+
+        // "Hi Mom"
+        $that = $(this);
+
+        // Go to the next slide
+        layers_next_onboarding_slide();
+
+    });
+
+    $(document).on( 'click' , '.layers-onboard-controllers a.btn-link.layers-button' , function(e){
+        e.preventDefault();
+
+        // "Hi Mom"
+        $that = $(this);
+
+        // Go to the next slide
+        layers_next_onboarding_slide();
+    });
+
     $(document).on( 'click' , '.onboard-nav-dots a' , function(e){
         e.preventDefault();
 
         // "Hi Mom"
         $that = $(this);
 
-        $that.addClass( 'dot-active' ).siblings().removeClass( 'dot-active' );
-
         $i = $that.index();
 
-        $( '.layers-onboard-slide' ).eq( $i ).addClass( 'layers-onboard-slide-current' ).removeClass( 'layers-onboard-slide-inactive' ).siblings().removeClass( 'layers-onboard-slide-current' ).addClass( 'layers-onboard-slide-inactive' );
-
+        layers_change_onboarding_slide( $i );
     });
+
+    function layers_next_onboarding_slide(){
+        $current = $( '.onboard-nav-dots a.dot-active').index();
+        $next = (+$current+1);
+        $max = $( '.onboard-nav-dots a' ).length;
+
+        if( $next == $max ) return;
+
+        layers_change_onboarding_slide( $next );
+    }
+
+    function layers_change_onboarding_slide( $i ){
+        // Update anchor classes
+        $( '.onboard-nav-dots a').eq( $i ).addClass( 'dot-active' ).siblings().removeClass( 'dot-active' );
+
+        // Update slider classes
+        $( '.layers-onboard-slide' ).eq( $i ).addClass( 'layers-onboard-slide-current' ).removeClass( 'layers-onboard-slide-inactive' ).siblings().removeClass( 'layers-onboard-slide-current' ).addClass( 'layers-onboard-slide-inactive' );
+    }
 });

b195e09f071735e89244cf50c97ba8e4de44d0ff
Marc Perel <marc@obox.co.za>
2015-02-23 11:24:53 +0200

    Replaced the search form in 404

--- partials/content-empty.php
+++ partials/content-empty.php
@@ -10,8 +10,5 @@
 </header>
 <div class="story">
     <p><?php _e( 'Use the search form below to find the page you\'re looking for:' , 'layerswp' ) ; ?></p>
-    <form action="<?php home_url(); ?>" method="get" class="search-form">
-        <input type="text" name="s" id="s" class="search" maxlength="50" placeholder="<?php esc_attr_e( 'Enter Text Here', 'layerswp' ); ?>" />
-        <input type="submit" class="search_button" value="<?php esc_attr_e( 'Search', 'gigawatt' ); ?>" />
-    </form>
+    <?php get_search_form(); ?>
 </div>

4c226ee8a8b8b9a4f0116dd26fbca358dc7e3892
= <mpretty@voceconnect.com>
2015-02-26 10:12:06 -0500

    fixing formatting

--- 404.php
+++ 404.php
@@ -5,20 +5,20 @@
  * @package Layers
  * @since Layers 1.0.0
  */
-
-get_header(); ?>
+get_header();
+?>
 
 <section class="post content-main clearfix container">
-    <div class="row">
+	<div class="row">
 
-        <?php get_sidebar( 'left' ); ?>
+		<?php get_sidebar( 'left' ); ?>
 
-        <article <?php layers_center_column_class(); ?>>
-            <?php get_template_part( 'partials/content', 'empty' ); ?>
-        </article>
+		<article <?php layers_center_column_class(); ?>>
+			<?php get_template_part( 'partials/content', 'empty' ); ?>
+		</article>
 
-        <?php get_sidebar( 'right' ); ?>
-    </div>
+		<?php get_sidebar( 'right' ); ?>
+	</div>
 </section>
 
 <?php get_footer(); ?>

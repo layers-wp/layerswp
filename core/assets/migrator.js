97108ce1ddcaa54174a97e4bdd417244aa222150
Rob Glazebrook <rglazebrook@gmail.com>
2015-02-26 17:07:42 -0500

    Updating delete-preset to remove the item from the DOM when it is hidden.

--- core/assets/migrator.js
+++ core/assets/migrator.js
@@ -264,7 +264,7 @@ console.log( attachment );
                 function(results){
                     $parent = $that.parents('.layers-product');
                     if ( results.success ) {
-                        $parent.fadeOut();
+                        $parent.fadeOut(500, function() { $(this).remove(); });
                     } else {
                         $parent.addClass('layers-ajax-error');
                     }

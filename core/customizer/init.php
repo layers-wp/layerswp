358a53d11e5bf71060c801afe6816aa5e66c2b13
= <mpretty@voceconnect.com>
2015-02-26 10:46:49 -0500

    fixing formatting

--- core/customizer/init.php
+++ core/customizer/init.php
@@ -1,4 +1,6 @@
-<?php /**
+<?php
+
+/**
  * Customizer Initiation File
  *
  * This file is the source of the Customizer functionality in Layers.
@@ -6,29 +8,27 @@
  * @package Layers
  * @since Layers 1.0.0
  */
-
 class Layers_Customizer {
 
 	private static $instance;
 
 	/**
-	*  Retrieve static/global instance of the Layers Customizer
-	*/
-
-	public static function get_instance(){
-		if ( ! isset( self::$instance ) ) {
+	 *  Retrieve static/global instance of the Layers Customizer
+	 */
+	public static function get_instance() {
+		if ( !isset( self::$instance ) ) {
 			self::$instance = new Layers_Customizer();
 		}
 		return self::$instance;
 	}
 
 	/**
-	*  Constructor
-	*/
-
+	 *  Constructor
+	 */
 	public function __construct() {
+		
 	}
-	
+
 	/**
 	 * Initializes the instance
 	 * @global type $wp_customize
@@ -45,7 +45,7 @@ class Layers_Customizer {
 		// Include The Default Settings Class
 		require_once get_template_directory() . $customizer_dir . 'defaults.php';
 
-		if( isset( $wp_customize ) ) {
+		if ( isset( $wp_customize ) ) {
 			// Include The Panel and Section Registration Class
 			require_once get_template_directory() . $customizer_dir . 'registration.php';
 
@@ -65,90 +65,84 @@ class Layers_Customizer {
 			// If we are in a builder page, update the Widgets title
 			$wp_customize->add_panel(
 				'widgets', array(
-					'priority' => 0,
-					'title' => __('Edit Layout' , 'layerswp' ),
-					'description' => $this->render_builder_page_dropdown() . __('Use this area to add widgets to your page, use the (Layers) widgets for the Body section.' , 'layerswp' ),
+				'priority' => 0,
+				'title' => __( 'Edit Layout', 'layerswp' ),
+				'description' => $this->render_builder_page_dropdown() . __( 'Use this area to add widgets to your page, use the (Layers) widgets for the Body section.', 'layerswp' ),
 				)
 			);
 
 			// Enqueue Styles
-			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) , 50 );
-			add_action( 'admin_enqueue_scripts', array( $this, 'admin_print_styles' ) , 50 );
-			add_action( 'customize_controls_print_styles' , array( $this, 'admin_print_styles' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 50 );
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_print_styles' ), 50 );
+			add_action( 'customize_controls_print_styles', array( $this, 'admin_print_styles' ) );
 
 			// Render header actions button(s)
-			add_action( 'customize_controls_print_footer_scripts' , array( $this, 'render_actions_buttons' ) );
+			add_action( 'customize_controls_print_footer_scripts', array( $this, 'render_actions_buttons' ) );
 		}
 	}
 
 	/**
-	*  Enqueue Widget Scripts
-	*/
-
-	public function admin_enqueue_scripts(){
+	 *  Enqueue Widget Scripts
+	 */
+	public function admin_enqueue_scripts() {
 
 		// Media Uploader required scripts
 		wp_enqueue_media();
 
 		// Customizer general
 		wp_enqueue_script(
-			LAYERS_THEME_SLUG . '-admin-customizer' ,
-			get_template_directory_uri() . '/core/customizer/js/customizer.js' ,
-			array(
-				'backbone',
-				'jquery',
-				'wp-color-picker'
-			),
-			LAYERS_VERSION,
-			true
+			LAYERS_THEME_SLUG . '-admin-customizer', get_template_directory_uri() . '/core/customizer/js/customizer.js', array(
+			'backbone',
+			'jquery',
+			'wp-color-picker'
+			), LAYERS_VERSION, true
 		);
 
 		// Localize Scripts
-		wp_localize_script( LAYERS_THEME_SLUG . '-admin-customizer' , "layers_customizer_params", array(
-									'ajaxurl' => admin_url( "admin-ajax.php" ) ,
-									'nonce' => wp_create_nonce( 'layers-customizer-actions' ),
-									'builder_page' => ( isset( $_GET[ 'layers-builder' ] ) ? TRUE : FALSE )
-								)
-							);
+		wp_localize_script( LAYERS_THEME_SLUG . '-admin-customizer', "layers_customizer_params", array(
+			'ajaxurl' => admin_url( "admin-ajax.php" ),
+			'nonce' => wp_create_nonce( 'layers-customizer-actions' ),
+			'builder_page' => ( isset( $_GET['layers-builder'] ) ? TRUE : FALSE )
+			)
+		);
 	}
 
 	/**
-	*  Enqueue Widget Styles
-	*/
-
-	public function admin_print_styles(){
+	 *  Enqueue Widget Styles
+	 */
+	public function admin_print_styles() {
 
 		// Widget styles
 		wp_register_style(
-			LAYERS_THEME_SLUG . '-admin-customizer',
-			get_template_directory_uri() . '/core/customizer/css/customizer.css',
-			array(),
-			LAYERS_VERSION
+			LAYERS_THEME_SLUG . '-admin-customizer', get_template_directory_uri() . '/core/customizer/css/customizer.css', array(), LAYERS_VERSION
 		);
 		wp_enqueue_style( LAYERS_THEME_SLUG . '-admin-customizer' );
 	}
 
 	/**
-	*  Render the dropdown of builder pages in Customizer interface.
-	*/
-
-	public function render_builder_page_dropdown(){
+	 *  Render the dropdown of builder pages in Customizer interface.
+	 */
+	public function render_builder_page_dropdown() {
 		global $wp_customize;
 
-		if(!$wp_customize) return;
+		if ( !$wp_customize )
+			return;
 
 		//Get builder pages.
 		$layers_pages = layers_get_builder_pages();
 
 		// Create builder pages dropdown.
-		if( $layers_pages ){
-			ob_start(); ?>
+		if ( $layers_pages ) {
+			ob_start();
+			?>
 			<div class="layers-customizer-pages-dropdown">
 				<select>
-					<option value="init"><?php _e( 'Builder Pages:' , 'layerswp' ) ?></option>
-					<?php foreach( $layers_pages as $page ) { ?>
-						<?php // Page URL
-						$edit_page_url = get_permalink( $page->ID ); ?>
+					<option value="init"><?php _e( 'Builder Pages:', 'layerswp' ) ?></option>
+					<?php foreach ( $layers_pages as $page ) { ?>
+						<?php
+						// Page URL
+						$edit_page_url = get_permalink( $page->ID );
+						?>
 						<option value="<?php echo esc_attr( $edit_page_url ); ?>"><?php echo $page->post_title ?></option>
 					<?php } ?>
 				</select>
@@ -162,21 +156,22 @@ class Layers_Customizer {
 		}
 	}
 
-	function render_actions_buttons() { ?>
+	public function render_actions_buttons() {
+		?>
 
-			<a class="customize-controls-layers-button customize-controls-layers-button-dashboard dashicons dashicons-plus layers-tooltip" href="<?php echo admin_url( 'admin.php?page=layers-add-new-page' ); ?>">
-				<span class="layers-tooltip-text layers-tooltip-text-large"><?php _e( 'Add new Layers page' , 'layerswp' ) ?></span>
-			</a>
-			<a class="customize-controls-layers-button customize-controls-layers-button-preview icon-display layers-tooltip" href="#" target="_blank">
-				<span class="layers-tooltip-text"><?php _e( 'Preview this page' , 'layerswp' ) ?></span>
-			</a>
+		<a class="customize-controls-layers-button customize-controls-layers-button-dashboard dashicons dashicons-plus layers-tooltip" href="<?php echo admin_url( 'admin.php?page=layers-add-new-page' ); ?>">
+			<span class="layers-tooltip-text layers-tooltip-text-large"><?php _e( 'Add new Layers page', 'layerswp' ) ?></span>
+		</a>
+		<a class="customize-controls-layers-button customize-controls-layers-button-preview icon-display layers-tooltip" href="#" target="_blank">
+			<span class="layers-tooltip-text"><?php _e( 'Preview this page', 'layerswp' ) ?></span>
+		</a>
 
-			<span class="layers-tooltip-text layers-tooltip-text-close layers-tooltip-text-tiny">
-				<?php _e( 'Close' , 'layerswp' ) ?>
-			</span>
-			<span class="layers-tooltip-text layers-tooltip-text-back">
-				<?php _e( 'Back to controls' , 'layerswp' ) ?>
-			</span>
+		<span class="layers-tooltip-text layers-tooltip-text-close layers-tooltip-text-tiny">
+			<?php _e( 'Close', 'layerswp' ) ?>
+		</span>
+		<span class="layers-tooltip-text layers-tooltip-text-back">
+			<?php _e( 'Back to controls', 'layerswp' ) ?>
+		</span>
 
 		<?php
 	}
@@ -184,12 +179,12 @@ class Layers_Customizer {
 }
 
 /**
-*  Kicking this off with the 'widgets_init' hook
-*/
-
-function layers_customizer_init(){
+ *  Kicking this off with the 'widgets_init' hook
+ */
+function layers_customizer_init() {
 	$layers_widget = new Layers_Customizer();
 	$layers_widget->init();
 }
-add_action( 'customize_register' , 'layers_customizer_init' , 50 );
-add_action( 'init' , 'layers_customizer_init');
+
+add_action( 'customize_register', 'layers_customizer_init', 50 );
+add_action( 'init', 'layers_customizer_init' );

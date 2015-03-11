c18bcf495fff50bf6a88e6f8547d37cebeb324f1
Marc Perel <marc@obox.co.za>
2015-03-11 15:29:32 +0200

    Fixed top menu fallbacks

--- partials/header-secondary.php
+++ partials/header-secondary.php
@@ -3,11 +3,11 @@
         <?php do_action( 'layers_before_header_secondary_inner' ); ?>
             <div class="<?php if( 'layout-fullwidth' != layers_get_theme_mod( 'header-width' ) ) echo 'container'; ?> clearfix">
                 <?php do_action( 'layers_before_header_secondary_left_nav' ); ?>
-                <?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-secondary-left' ,'container' => 'nav', 'container_class' => 'pull-left' , 'menu_class' => 'nav nav-horizontal', 'fallback_cb' => 'layers_menu_fallback' )); ?>
+                <?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-secondary-left' ,'container' => 'nav', 'container_class' => 'pull-left' , 'menu_class' => 'nav nav-horizontal', 'fallback_cb' => FALSE )); ?>
                 <?php do_action( 'layers_after_header_secondary_left_nav' ); ?>
 
                 <?php do_action( 'layers_before_header_secondary_right_nav' ); ?>
-                <?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-secondary-right' ,'container' => 'nav', 'container_class' => 'pull-right' , 'menu_class' => 'nav nav-horizontal', 'fallback_cb' => false )); ?>
+                <?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-secondary-right' ,'container' => 'nav', 'container_class' => 'pull-right' , 'menu_class' => 'nav nav-horizontal', 'fallback_cb' => FALSE )); ?>
                 <?php do_action( 'layers_after_header_secondary_right_nav' ); ?>
             </div>
         <?php do_action( 'layers_after_header_secondary_inner' ); ?>

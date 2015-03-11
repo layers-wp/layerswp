b141f41f4e4842a0693103b717121b72e5292776
Marc Perel <marc@obox.co.za>
2015-03-11 15:19:09 +0200

    Added license attribution to swiper.js

--- core/widgets/js/swiper.js
+++ core/widgets/js/swiper.js
@@ -1,3 +1,17 @@
+/*
+ * Swiper 2.7.6
+ * Mobile touch slider and framework with hardware accelerated transitions
+ *
+ * http://www.idangero.us/sliders/swiper/
+ *
+ * Copyright 2010-2015, Vladimir Kharlampidi
+ * The iDangero.us
+ * http://www.idangero.us/
+ *
+ * Licensed under GPL & MIT
+ *
+ * Released on: February 11, 2015
+*/
 var Swiper = function (selector, params) {
     'use strict';
 

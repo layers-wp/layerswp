12fe84473c0712a071ce7c787c15812dbe593f3d
Stuart <stuart@calyxagency.com>
2015-03-17 15:13:10 +0200

    make sure layers-custom-css is always on the page

--- core/helpers/template.php
+++ core/helpers/template.php
@@ -371,17 +371,16 @@ if( !function_exists( 'layers_apply_customizer_styles' ) ) {
 		global $wp_customize;
 
 		// Custom CSS
-		if( layers_get_theme_mod( 'custom-css' ) ){
-			
-			if ( isset( $wp_customize ) ) {
-				?>
-				<style id="layers-custom-css">
-				<?php echo layers_get_theme_mod( 'custom-css' ); ?>
-				</style>
-				<?php
-			} else {
-				layers_inline_styles( NULL, 'css', array( 'css' => layers_get_theme_mod( 'custom-css' ) ) );
-			}
+		if ( isset( $wp_customize ) ) {
+			// Don't inline css if in previewer window
+			?>
+			<style id="layers-custom-css">
+			<?php echo esc_html( layers_get_theme_mod( 'custom-css' ) ); ?>
+			</style>
+			<?php
+		}
+		else if ( layers_get_theme_mod( 'custom-css' ) ){
+			layers_inline_styles( NULL, 'css', array( 'css' => layers_get_theme_mod( 'custom-css' ) ) );
 		}
 
 		// Header

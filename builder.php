4f363ffb15d274ee829efff243947bbe838d29ea
Marc Perel <marc@obox.co.za>
2015-04-15 15:29:42 +0200

    Making Layers work with private pages

--- builder.php
+++ builder.php
@@ -14,20 +14,19 @@ global $post;
 do_action('before_layers_builder_widgets');
 
 if ( post_password_required() ) { ?>
-    <section id="post-<?php the_ID(); ?>" <?php post_class( 'content-main clearfix' ); ?>>
-        <?php do_action('layers_before_post_loop'); ?>
-        <div class="row">
-            <?php do_action('layers_before_private_post'); ?>
-            <div class="story">
-                <?php echo get_the_password_form(); ?>
-            </div>
-            <?php do_action('layers_after_private_post'); ?>
-        </div>
-        <?php do_action('layers_after_post_loop'); ?>
-    </section>
-
+	<section id="post-<?php the_ID(); ?>" <?php post_class( 'content-main clearfix' ); ?>>
+		<?php do_action('layers_before_post_loop'); ?>
+		<div class="row">
+			<?php do_action('layers_before_private_post'); ?>
+			<div class="story">
+				<?php echo get_the_password_form(); ?>
+			</div>
+			<?php do_action('layers_after_private_post'); ?>
+		</div>
+		<?php do_action('layers_after_post_loop'); ?>
+	</section>
 <?php } else {
-    dynamic_sidebar( 'obox-layers-builder-' . $post->ID );
+	dynamic_sidebar( 'obox-layers-builder-' . $post->ID );
 }
 
 do_action('after_layers_builder_widgets');

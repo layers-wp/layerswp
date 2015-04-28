0092240c9da198a392c7f2ccc1343492426994de
Stuart <stuart@calyxagency.com>
2015-04-28 12:06:21 +0200

    invert comments color fixing

--- core/helpers/post.php
+++ core/helpers/post.php
@@ -118,7 +118,7 @@ if( !function_exists( 'layers_comment' ) ) {
 		<?php if( 2  < $depth && isset( $GLOBALS['lastdepth'] ) && $depth != $GLOBALS['lastdepth'] ) { ?>
 			<div class="row comments-nested push-top">
 		<?php } ?>
-		<div <?php comment_class( 'content push-bottom well' ); ?> id="comment-<?php comment_ID(); ?>">
+		<div <?php comment_class( 'content well' ); ?> id="comment-<?php comment_ID(); ?>">
 			<div class="avatar push-bottom clearfix">
 				<?php edit_comment_link(__('(Edit)' , 'layerswp' ),'<small class="pull-right">','</small>') ?>
 				<a class="avatar-image" href="">

ecd223806e0dc6797af328a22b4c3ae852882c62
Marc Perel <marc@obox.co.za>
2015-06-26 14:55:14 +0200

    Reversed some hunks :D

--- core/customizer/config.php
+++ core/customizer/config.php
@@ -410,7 +410,7 @@ class Layers_Customizer_Config {
 		// Footer -> Layout -> Text
 		$controls['footer-text'] = array(
 				'footer-copyright-text' => array(
-					'type'     => 'layers-rte',
+					'type'     => 'layers-text',
 					'label'    => __( 'Copyright Text' , 'layerswp' ),
 					'default' => ' Made at the tip of Africa. &copy;',
 				), // copyright

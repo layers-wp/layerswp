3a2aef139d1fc9303cf0e87fb5fec72d619e044c
Marc Perel <marc@obox.co.za>
2015-07-17 11:22:55 +0200

    Updated widget constructors

--- core/widgets/modules/slider.php
+++ core/widgets/modules/slider.php
@@ -40,7 +40,7 @@ if( !class_exists( 'Layers_Slider_Widget' ) ) {
 			$control_ops = array( 'width' => LAYERS_WIDGET_WIDTH_LARGE, 'height' => NULL, 'id_base' => LAYERS_THEME_SLUG . '-widget-' . $this->widget_id );
 
 			/* Create the widget. */
-			$this->WP_Widget( LAYERS_THEME_SLUG . '-widget-' . $this->widget_id , $this->widget_title, $widget_ops, $control_ops );
+			parent::__construct( LAYERS_THEME_SLUG . '-widget-' . $this->widget_id , $this->widget_title, $widget_ops, $control_ops );
 
 			/* Setup Widget Defaults */
 			$this->defaults = array (

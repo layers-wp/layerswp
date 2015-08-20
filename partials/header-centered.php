8e7f8157c6b5eb0aee9032a36eb4b383776c52d7
Asif Iqbal <asif@adommo.com>
2015-08-21 04:34:56 +0600

    Bug Fix Issue#74
    Bug Fix accordiing to issue#74

--- partials/header-centered.php
+++ partials/header-centered.php
@@ -13,7 +13,7 @@
 
 <div class="column inline-right-nav">
 	<?php do_action( 'layers_before_header_nav' ); ?>
-	<?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-primary-right' ,'container' => 'nav', 'container_class' => 'nav nav-horizontal', 'fallback_cb' => create_function('', 'echo "&nbsp";') ) ); ?>
+	<?php wp_nav_menu( array( 'theme_location' => LAYERS_THEME_SLUG . '-primary-right' ,'container' => 'nav', 'container_class' => 'nav nav-horizontal', 'fallback_cb' => false )); ?>
 	<?php get_template_part( 'partials/responsive' , 'nav-button' ); ?>
 	<?php do_action( 'layers_after_header_nav' ); ?>
 </div>

ff199a1352fa0ef70fa9c6d25c55c4eaa60d63ba
Marc Perel <marc@obox.co.za>
2015-09-08 11:29:18 +0200

    Fixed category lists

--- archive-product.php
+++ archive-product.php
@@ -12,37 +12,37 @@ get_header(); ?>
 
 <section class="<?php if( 'layout-fullwidth' != layers_get_theme_mod( 'content-layout-layout' ) ) echo 'container'; ?> clearfix content-main row">
 
-        <?php /**
-        * Maybe show the left sidebar
-        */
-        layers_maybe_get_sidebar( 'left-woocommerce-sidebar', implode( ' ', layers_get_wrapper_class( 'body_sidebar', 'column pull-left sidebar span-3' ) ) ); ?>
-        <?php if ( have_posts()) : ?>
-            <section <?php layers_center_column_class(); ?>>
+		<?php /**
+		* Maybe show the left sidebar
+		*/
+		layers_maybe_get_sidebar( 'left-woocommerce-sidebar', implode( ' ', layers_get_wrapper_class( 'body_sidebar', 'column pull-left sidebar span-3' ) ) ); ?>
+		<?php if ( have_posts()) : ?>
+			<section <?php layers_center_column_class(); ?>>
 
-                <div class="row push-bottom woocommerce-result-count-container">
-                    <?php  do_action('woocommerce_before_shop_loop'); ?>
-                </div>
+				<div class="row push-bottom woocommerce-result-count-container">
+					<?php  do_action('woocommerce_before_shop_loop'); ?>
+				</div>
 
-                <ul class="products row">
+				<ul class="products row">
 
-                    <?php // Sub category listing
-                    woocommerce_product_subcategories(); ?>
+					<?php // Sub category listing
+					woocommerce_product_subcategories(); ?>
 
-                    <?php while (have_posts()) :  the_post(); ?>
-                            <?php woocommerce_get_template_part( 'content' , 'product' ); ?>
-                    <?php endwhile; ?>
-                </ul>
+					<?php while (have_posts()) :  the_post(); ?>
+							<?php woocommerce_get_template_part( 'content' , 'product' ); ?>
+					<?php endwhile; ?>
+				</ul>
 
-                <?php layers_pagination(); ?>
+				<?php layers_pagination(); ?>
 
-                <?php woocommerce_product_loop_end(); ?>
+				<?php woocommerce_product_loop_end(); ?>
 
-            </section>
-        <?php endif; ?>
+			</section>
+		<?php endif; ?>
 
-        <?php /**
-        * Maybe show the right sidebar
-        */
-        layers_maybe_get_sidebar( 'right-woocommerce-sidebar', implode( ' ', layers_get_wrapper_class( 'body_sidebar', 'column pull-right sidebar span-3 no-gutter' ) ) ); ?>
+		<?php /**
+		* Maybe show the right sidebar
+		*/
+		layers_maybe_get_sidebar( 'right-woocommerce-sidebar', implode( ' ', layers_get_wrapper_class( 'body_sidebar', 'column pull-right sidebar span-3 no-gutter' ) ) ); ?>
 </section>
 <?php get_footer();

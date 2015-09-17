493360a0d9b2ff132b85658e86f06d5472b34c08
Marc Perel <marc@obox.co.za>
2015-09-17 15:23:48 +0200

    Added the DISABLE MARKETPLACE variable around the media tab query

--- core/options-panel/init.php
+++ core/options-panel/init.php
@@ -44,11 +44,11 @@ class Layers_Options_Panel {
 
 		add_action( 'wp_dashboard_setup', array( &$this, 'layers_add_dashboard_widgets' ) );
 
-		// add the tab
-
-		add_filter('media_upload_tabs', array( &$this, 'upload_upsell_media_tab' ) );
-		add_action('media_upload_upsell_media', array( &$this, 'upload_upsell_media_form' ) );
-		add_action( 'print_media_templates', array( &$this, 'upload_upsell_media_template' ) );
+		if( !defined( 'LAYERS_DISABLE_MARKETPLACE' ) ){
+			add_filter( 'media_upload_tabs', array( &$this, 'upload_upsell_media_tab' ) );
+			add_action( 'media_upload_upsell_media', array( &$this, 'upload_upsell_media_form' ) );
+			add_action( 'print_media_templates', array( &$this, 'upload_upsell_media_template' ) );
+		}
 	}
 
 	public function init() {

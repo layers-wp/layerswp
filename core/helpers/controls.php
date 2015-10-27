2a947d955f10c704776b531f768cd958c49b176d
Stuart <stuart@calyxagency.com>
2015-10-27 10:47:09 +0200

    add control helpers for modifying customizer control config

--- core/helpers/controls.php
+++ core/helpers/controls.php
@@ -1,7 +1,40 @@
 <?php
+/**
+ * Helper function to assist with modifying the Customizer Controls (add, remove, etc).
+ *
+ */
 
 /**
- * Insert controls after specific controls in the config array - for use in the Widgets.
+ * Insert controls before specific controls in the config array.
+ *
+ * @param    string $key           Key of the elemt that you want to insert-before.
+ * @param    array $config         The existing config array to be modified.
+ * @param    array $new_elements   Nested array containing new field elements.
+ * @return   array                 The newly modified config array
+ */
+if( ! function_exists( 'layers_insert_config_elements_before' ) ) {
+	function layers_insert_config_elements_before( $key, $config, $new_elements ) {
+		
+		foreach ( $config as $group_key => $group_array ) {
+			
+			if ( isset( $group_array[$key] ) ) {
+				
+				$offset = ( array_search( $key, array_keys( $group_array ) ) );
+				
+				$group_array =	array_slice( $group_array, 0, $offset, true ) +
+								$new_elements +
+								array_slice( $group_array, $offset, count( $group_array ) - 1, true );
+								
+				$config[$group_key] = $group_array;
+			}
+		}
+		
+		return $config;
+	}
+}
+
+/**
+ * Insert controls after specific controls in the config array.
  *
  * @param    string $key           Key of the elemt that you want to insert-after.
  * @param    array $config         The existing config array to be modified.
@@ -11,13 +44,43 @@
 if( ! function_exists( 'layers_insert_config_elements_after' ) ) {
 	function layers_insert_config_elements_after( $key, $config, $new_elements ) {
 		
-		if ( isset( $config[$key] ) ) {
+		foreach ( $config as $group_key => $group_array ) {
 			
-			$offset = array_search( $key, array_keys( $config ) );
+			if ( isset( $group_array[$key] ) ) {
+				
+				$offset = ( array_search( $key, array_keys( $group_array ) ) ) + 1;
+				
+				$group_array =	array_slice( $group_array, 0, $offset, true ) +
+								$new_elements +
+								array_slice( $group_array, $offset, count( $group_array ) - 1, true );
+								
+				$config[$group_key] = $group_array;
+			}
+		}
+		
+		return $config;
+	}
+}
+
+/**
+ * Insert controls after specific controls in the config array.
+ *
+ * @param    string $key           Key of the elemt that you want to insert-after.
+ * @param    array $config         The existing config array to be modified.
+ * @param    array $new_elements   Nested array containing new field elements.
+ * @return   array                 The newly modified config array
+ */
+if( ! function_exists( 'layers_remove_config_element' ) ) {
+	function layers_remove_config_element( $key, $config ) {
+		
+		foreach ( $config as $group_key => $group_array ) {
 			
-			$config =	array_slice( $config, 0, $offset, true ) +
-						$new_elements +
-    					array_slice( $config, $offset, count( $config ) - 1, true );
+			if ( isset( $group_array[$key] ) ) {
+				
+				unset( $group_array[$key] );
+								
+				$config[$group_key] = $group_array;
+			}
 		}
 		
 		return $config;

126be22b68b7f482a2008a2d6172bc3b998eb668
Stuart <stuart@calyxagency.com>
2015-11-30 15:57:43 +0200

    allow new sync_setting prop

--- core/helpers/forms.php
+++ core/helpers/forms.php
@@ -124,6 +124,7 @@ class Layers_Form_Elements {
 			'value' => NULL ,
 			'class' => NULL,
 			'options' => array(),
+			'sync_setting' => NULL,
 		);
 
 		// Convert 'choices' to 'options' - so you can use same naming as the controls which use 'options'.
@@ -147,6 +148,7 @@ class Layers_Form_Elements {
 		$input_props['placeholder'] = ( NULL !== $input->placeholder ) ? 'placeholder="' . esc_attr( $input->placeholder ) . '"' : NULL ;
 		$input_props['class'] = ( NULL != $input->class ) ? 'class="' .  $input->class . '"' : NULL ;
 		$input_props['disabled'] = isset( $input->disabled ) ? 'disabled="disabled"' : NULL ;
+		$input_props['sync-setting'] = isset( $input->sync_setting ) ? 'data-sync-setting="' . $input->sync_setting . '"' : NULL ;
 
 		if( NULL != $input->data ) { foreach( $input->data as $data_key => $data_value ){ $input_props[ 'data-' . $data_key ] = 'data-' . $data_key . '="' . esc_attr( $data_value ) . '"'; } }
 

e80d6af5395a1af99d3c0aec3d627a3b47916435
Stuart <stuart@calyxagency.com>
2016-01-22 12:12:55 +0200

    fix `show-if-value` functionality in the customizer controls

--- core/customizer/controls/rte.php
+++ core/customizer/controls/rte.php
@@ -19,7 +19,7 @@ if( !class_exists( 'Layers_Customize_RTE_Control' ) ) {
 
 			$values = false; ?>
 
-			<div data-test id="layers-customize-control-<?php echo esc_attr( $this->id ); ?>" class="l_option-customize-control l_option-customize-control-<?php echo esc_attr( str_replace( 'layers-', '', $this->type ) ); ?> <?php echo esc_attr( $this->class ); ?>" <?php echo $this->get_linked_data(); ?> >
+			<div id="layers-customize-control-<?php echo esc_attr( $this->id ); ?>" class="l_option-customize-control l_option-customize-control-<?php echo esc_attr( str_replace( 'layers-', '', $this->type ) ); ?> <?php echo esc_attr( $this->class ); ?>" <?php echo $this->get_linked_data(); ?> >
 
 				<?php $this->render_history_actions(); ?>
 

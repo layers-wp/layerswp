33019472217c0518f417fe6dc5e823ccd8c9dd45
Marc Perel <marc@obox.co.za>
2016-04-04 13:37:26 +0200

    Improved WordPress 4.5 Site Logo Support

--- core/third-party/site-logo.php
+++ core/third-party/site-logo.php
@@ -2,6 +2,7 @@
 	function layers_site_logo_init() {
 		// Only load our code if our theme declares support, and the standalone plugin is not activated.
 		if ( current_theme_supports( 'site-logo' ) && ! class_exists( 'Site_Logo', false ) ) {
+
 			// Load our class for namespacing.
 			require_once( dirname( __FILE__ ) . '/site-logo/inc/class-site-logo.php' );
 

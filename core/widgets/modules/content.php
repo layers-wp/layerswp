c3949b33dfd86feca20a99aee87825a2dced3931
Marc Perel <marc@obox.co.za>
2016-04-12 17:15:36 +0200

    Added support for filtering classes on widget container and items

--- core/widgets/modules/content.php
+++ core/widgets/modules/content.php
@@ -282,6 +282,8 @@ if( !class_exists( 'Layers_Content_Widget' ) ) {
 								if( false != $media ) {
 									$classes[] = 'has-image';
 								}
+
+								$classes = apply_filters( 'layers_content_widget_item_class', $classes, $this, $item, $widget );
 								$classes = implode( ' ', $classes ); ?>
 
 								<div id="<?php echo $widget_id; ?>-<?php echo $column_key; ?>" class="<?php echo esc_attr( $classes ); ?>">

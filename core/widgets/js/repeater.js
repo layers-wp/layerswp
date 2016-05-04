bf42530c23b47d5341dbebe397e65e6c24a1aa23
Stuart <stuart@calyxagency.com>
2016-05-04 15:36:25 +0200

    create a js plugin called `layersRepeater()` so that a method `add_item()` can be called by our extensions.

--- core/widgets/js/repeater.js
+++ core/widgets/js/repeater.js
@@ -17,30 +17,25 @@
 */
 
 jQuery(document).ready(function($){
+	
+	$.fn.layersRepeater = function( options ) {
 
-	/**
-	* 1 - Sortable items
-	*/
+		// Apply defaults to the options
+        $.extend( options, options, {});
 
-	$( document ).on( 'layers-interface-init', function( e, element ){
-		layers_set_column_sortable( $(element) );
-	});
+		return this.each(function() {
 
-	function layers_set_column_sortable( $element_s ){
+			var $repeater = $(this);
 
-		$element_s.find( 'ul.layers-accordions-sortable' ).each( function(){
-
-			$that = $(this);
-
-			$that.sortable({
+			$repeater.find( 'ul.layers-accordions-sortable' ).sortable({
 				placeholder: "layers-sortable-drop",
 				handle: ".layers-accordion-title",
 				stop: function(e , li){
 
 					// Get Elements
 					$repeater_list = li.item.closest( 'ul' );
-					$repeater_group = $repeater_list.parents('.layers-widget-repeater');
-					$repeater_input = $repeater_group.find('.layers-repeater-input');
+					$repeater = $repeater_list.parents('.layers-widget-repeater');
+					$repeater_input = $repeater.find('.layers-repeater-input');
 
 					// Apply new column order
 					$item_guids = [];
@@ -52,12 +47,114 @@ jQuery(document).ready(function($){
 					$repeater_input.val( $item_guids.join() ).layers_trigger_change();
 				}
 			});
+
+			var repeater_object = {
+				add_item : function( args ){
+					
+					// Get elements
+					$repeater            = $repeater;
+					$repeater_add_button = $repeater.find('.layers-widget-repeater-add-item');
+					$repeater_list       = $repeater.find('.layers-accordions');
+					$repeater_input      = $repeater.find('.layers-repeater-input');
+					$widget_form         = $repeater.parents('.widget-content');
+
+					// Get data
+					var data_repeater_type    = $repeater.attr( 'data-repeater-type' );
+					var data_repeater_number  = $repeater.attr( 'data-repeater-number' );
+					var data_repeater_class   = $repeater.attr( 'data-repeater-class' );
+					var data_repeater_id_base = $repeater.attr( 'data-repeater-id-base' );
+
+					// Add loading class
+					$repeater_add_button.addClass('layers-loading-button');
+
+					// Serialize input data
+					$serialized_item_inputs = $repeater_list.find( 'li.layers-accordion-item' ).last().find( 'textarea, input, select, checkbox' ).serialize();
+
+					$post_data = {
+						action          : 'layers_widget_new_repeater_item',
+						widget_action   : 'add-item',
+						item_type       : data_repeater_type,
+						item_class      : data_repeater_class,
+						item_instance   : $serialized_item_inputs,
+						id_base         : data_repeater_id_base,
+						number          : data_repeater_number,
+						nonce           : layers_widget_params.nonce,
+					};
+					
+					// -
+					if ( null != args && args.hasOwnProperty('merge_instance') )
+						$post_data['merge_instance'] = args.merge_instance;
+					
+					// Merge the extra data into the post data - so they are available to the item_xxx() function.
+					if ( null != args && args.hasOwnProperty('merge_post_data') )
+						$.extend( $post_data, $post_data, args.merge_post_data );
+										
+					$.post(
+						ajaxurl,
+						$post_data,
+						function(data){
+
+							// Set item
+							$item = $(data);
+
+							$item.find('.layers-accordion-section').hide();
+
+							// Append item HTML
+							$repeater_list.append($item);
+
+							// Append item IDs to the items input
+							$item_guids = [];
+							$repeater_list.find( 'li.layers-accordion-item' ).each(function(){
+								$item_guids.push( $(this).data( 'guid' ) );
+							});
+
+							// Trigger change for ajax save
+							$repeater_input.val( $item_guids.join() ).layers_trigger_change();
+
+							// Trigger interface init. will trigger init of elements eg colorpickers etc
+							$(document).trigger( 'layers-interface-init', $item );
+
+							// Remove loading class
+							$repeater_add_button.removeClass('layers-loading-button');
+							
+							// Custom Complete Function - passed as an arg.
+							if ( null != args && args.hasOwnProperty('complete') )
+								args.complete();
+
+							// Add Open Class to item
+							setTimeout( function(){
+								$item.find('.layers-accordion-title').trigger('click');
+							}, 300 );
+						}
+					);
+
+				},
+			};
+			
+			// Save the repeater as data - so it can be accessed to execute methods.
+			$(this).data( 'layers_repeater', repeater_object );
+		});
+	};
+
+	/**
+	* 1 - Sort Items
+	*/
+
+	$( document ).on( 'layers-interface-init', function( e, element ){
+		layers_set_column_sortable( $(element) );
+	});
+
+	function layers_set_column_sortable( $element_s ){
+		
+		$element_s.find( '.layers-widget-repeater' ).each( function(){
+			$(this).layersRepeater();
 		});
 	}
 
 	/**
-	* 2 - Item Removal
+	* 2 - Remove Item
 	*/
+	
 	$(document).on( 'click' , '.layers-widget-repeater .layers-icon-error' , function(e){
 
 		e.preventDefault();
@@ -73,9 +170,9 @@ jQuery(document).ready(function($){
 		$repeater_delete_button = $(this);
 		$repeater_item_row      = $repeater_delete_button.closest( '.layers-accordion-item' );
 		$repeater_item_title    = $repeater_item_row.find('.layers-accordion-title');
-		$repeater_group         = $repeater_delete_button.parents('.layers-widget-repeater');
-		$repeater_list          = $repeater_group.find('.layers-accordions');
-		$repeater_input         = $repeater_group.find('.layers-repeater-input');
+		$repeater               = $repeater_delete_button.parents('.layers-widget-repeater');
+		$repeater_list          = $repeater.find('.layers-accordions');
+		$repeater_input         = $repeater.find('.layers-repeater-input');
 
 		// Close the row first.
 		$repeater_item_title.click();
@@ -105,7 +202,6 @@ jQuery(document).ready(function($){
 
 					// Reset Sortable Items
 					layers_set_column_sortable( $that );
-
 				}});
 
 			}, 700 );
@@ -114,95 +210,19 @@ jQuery(document).ready(function($){
 	});
 
 	/**
-	* 3 - Item Adittion
+	* 3 - Add Item
 	*/
-	$(document).on( 'click' , '.layers-widget-repeater-add-item' , function(e){
-
+	
+	$(document).on( 'click', '.layers-widget-repeater-add-item' , function(e){
+		
 		e.preventDefault();
-
-		// "Hi Mom"
-		var $that = $(this);
-
-		// Get elements
-		$repeater_add_button = $(this);
-		$repeater_group  = $repeater_add_button.parents('.layers-widget-repeater');
-		$repeater_list   = $repeater_group.find('.layers-accordions');
-		$repeater_input  = $repeater_group.find('.layers-repeater-input');
-		$widget_form     = $repeater_add_button.parents('.widget-content');
-
-		// Get data
-		var data_repeater_type    = $repeater_group.attr( 'data-repeater-type' );
-		var data_repeater_number  = $repeater_group.attr( 'data-repeater-number' );
-		var data_repeater_class   = $repeater_group.attr( 'data-repeater-class' );
-		var data_repeater_id_base = $repeater_group.attr( 'data-repeater-id-base' );
-
-		// Add loading class
-		$repeater_add_button.addClass('layers-loading-button');
-
-		// Serialize input data
-		$serialized_inputs = [];
-		$.each(
-			$repeater_list.find( 'li.layers-accordion-item' ).last().find( 'textarea, input, select' ),
-			function( i, input ){
-				$serialized_inputs.push( $(input).serialize() );
-			}
-		);
-
-		// Serialize input data
-		$serialized_instance_inputs = $widget_form.find( 'textarea, input, select, checkbox' ).serialize();
-
-		$post_data = {
-			action        : 'layers_widget_new_repeater_item',
-			widget_action : 'add-item',
-			item_type     : data_repeater_type,
-			item_class    : data_repeater_class,
-			instance      : $serialized_instance_inputs,
-			id_base       : data_repeater_id_base,
-			number        : data_repeater_number,
-			nonce         : layers_widget_params.nonce,
-		};
-
-		$.post(
-			ajaxurl,
-			$post_data,
-			function(data){
-
-				// Set item
-				$item = $(data);
-
-				$item.find('.layers-accordion-section').hide();
-
-				// Append item HTML
-				$repeater_list.append($item);
-
-				// Append item IDs to the items input
-				$item_guids = [];
-				$repeater_list.find( 'li.layers-accordion-item' ).each(function(){
-					$item_guids.push( $(this).data( 'guid' ) );
-				});
-
-				// Trigger change for ajax save
-				$repeater_input.val( $item_guids.join() ).layers_trigger_change();
-
-				// Trigger interface init. will trigger init of elemnts eg colorpickers etc
-				$( document ).trigger( 'layers-interface-init', $item );
-
-				// Remove loading class
-				$repeater_add_button.removeClass('layers-loading-button');
-
-				// Add Open Class to item
-				setTimeout( function(){
-					$item.find('.layers-accordion-title').trigger('click');
-				}, 300 );
-			}
-		) // $.post
-
+		$repeater = $(this).closest('.layers-widget-repeater');
+		$repeater_obj = $repeater.data( 'layers_repeater' );
+		$repeater_obj.add_item();
 	});
 
-
-
 	/**
-	* 3 - Module Title Update
+	* 3 - Update Title
 	*/
 
 	$(document).on( 'keyup' , 'ul[id^="column_list_"] input[id*="-title"]' , function(e){
@@ -218,8 +238,6 @@ jQuery(document).ready(function($){
 
 		// Update the accordian title
 		$that.closest( '.layers-accordion-item' ).find( 'span.layers-detail' ).text( $string );
-
 	});
-
 
 }); //jQuery

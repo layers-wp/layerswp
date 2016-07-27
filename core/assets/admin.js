4fdaa2c4a969dd799f9f9f6eee2526c6c2498e0b
stuartobrien <stuart@calyxagency.com>
2016-07-27 16:54:44 +0200

    spelling

--- core/assets/admin.js
+++ core/assets/admin.js
@@ -117,7 +117,7 @@ jQuery(function($) {
 			// Fade in Remove button
 			$container.find('.layers-image-remove').fadeIn();
 
-			// Set attachment to the larege/medium size if they're defined
+			// Set attachment to the large/medium size if they're defined
 			if ( undefined !== attachment.sizes.medium )  {
 				$attachment = attachment.sizes.medium;
 			} else if( undefined !== attachment.sizes.large ) {
@@ -478,7 +478,6 @@ jQuery(function($) {
 	*/
 
 	$.fn.layers_trigger_change = function() {
-
 		// Trigger 'change' and 'blur' to reset the customizer
 		$changed = $(this).trigger("change").trigger("blur");
 	};
@@ -568,7 +567,7 @@ jQuery(function($) {
 			$compare_element_value = $compare_element.val();
 		}
 
-		// Bail if there's no source element to refference.
+		// Bail if there's no source element to reference.
 		if ( 'undefined' === typeof( $compare_element_value ) || null === $compare_element_value ) {
 			layers_show_if_display( 'hide', $this_element );
 			return false;
@@ -767,7 +766,7 @@ jQuery(function($) {
 
 	}
 
-	// Fix for 'clear formatting' button not working - envokes sending change to customizer prev
+	// Fix for 'clear formatting' button not working - invokes sending change to customizer prev
 	$(document).on( 'click', '.fr-bttn[data-cmd="removeFormat"]', function(){
 		var $editor = $(this).closest('.layers-form-item').find('.layers-rte');
 		_.defer( function(arguments) {
@@ -794,7 +793,7 @@ jQuery(function($) {
 			// editor. So if the newly_focussed_element is not a form field then
 			// we help by invisibly focussing Froala's hidden sister textarea which
 			// releases the resource heavy Froala editor and returns performance
-			// to it's nomral state.
+			// to it's normal state.
 			if (
 					! $newly_focussed_element.is('input') &&
 					! $newly_focussed_element.is('textarea') &&
@@ -949,7 +948,7 @@ jQuery(function($) {
 
 			// If the range-slider is moved and there's a placeholder set
 			// and the slider stops on the placeholder value then empty
-			// the number field so ntohing is applied.
+			// the number field so nothing is applied.
 			$number_field.val('');
 			$number_field.addClass( 'layers-range-disabled' );
 		}
@@ -1216,7 +1215,7 @@ jQuery(function($) {
 				$collection_heading.html( display_content );
 			});
 
-			// Ping an intial update at the start.
+			// Ping an initial update at the start.
 			$(element).find('select, input').eq(0).trigger('layers_init_linking');
 		});
 	}

b4eaf73125a182857963d73401cc97c92754a681
Marc Perel <marc@Marcs-iMac.local>
2016-08-12 17:14:34 +0200

    Parallax upsell

--- core/helpers/design-bar.php
+++ core/helpers/design-bar.php
@@ -775,7 +775,7 @@ class Layers_Design_Controller {
 			),
 			'background-parallax' => array(
 				'type' => 'checkbox',
-				'label' => __( 'Parallax (Get with <a href="https://www.layerswp.com/layers-pro/?ref=obox&amp;utm_source=layers%20theme&amp;utm_medium=link&amp;utm_campaign=Layers%20Pro%20Upsell&amp;utm_content=Widget%20Parallax%20Upsell" target="_blank">Layers Pro</a>)', 'layerswp' ),
+				'label' => __( 'Parallax <span class="layers-inline-upsell">(Get with <a href="https://www.layerswp.com/layers-pro/?ref=obox&amp;utm_source=layers%20theme&amp;utm_medium=link&amp;utm_campaign=Layers%20Pro%20Upsell&amp;utm_content=Widget%20Parallax%20Upsell" target="_blank">Layers Pro</a>)</span>', 'layerswp' ),
 				'name' => $this->get_layers_field_name( 'background', 'stretch' ),
 				'id' => $this->get_layers_field_id( 'background', 'stretch' ),
 				'value' => ( isset( $this->values['background']['stretch'] ) ) ? $this->values['background']['stretch'] : NULL,

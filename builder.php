528910b08477a180ca4457ea5c5533a3e6da71a5
Marc Perel <marc@Marcs-iMac.local>
2016-09-06 10:59:04 +0200

    Implementing sidebars into the front-end on builder pages

--- builder.php
+++ builder.php
@@ -25,7 +25,37 @@ if ( post_password_required() ) { ?>
 		<?php do_action('layers_after_post_loop'); ?>
 	</div>
 <?php } else {
-	dynamic_sidebar( 'obox-layers-builder-' . $post->ID );
+
+	// Get the page layout
+	$page_layout = get_post_meta( get_the_ID(), 'layers_page_layout' , true );
+
+	switch( $page_layout ){
+		case 'left-sidebar' :
+			$show_left_sidebar = true;
+			$show_right_sidebar = false;
+			$center_col_cass = 'span-8';
+			break;
+		case 'right-sidebar' :
+			$show_left_sidebar = false;
+			$show_right_sidebar = true;
+			$center_col_cass = 'span-8';
+			break;
+		case 'left-right-sidebar' :
+			$show_left_sidebar = true;
+			$show_right_sidebar = true;
+			$center_col_cass = 'span-6';
+			break;
+		default:
+			$show_left_sidebar = false;
+			$show_right_sidebar = false;
+			$center_col_cass = 'span-12';
+			break;
+	}
+	if( $show_left_sidebar ) get_sidebar( 'left' ); ?>
+	<div class="<?php echo $center_col_cass; ?>">
+		<?php dynamic_sidebar( 'obox-layers-builder-' . $post->ID ); ?>
+	</div>
+	<?php if( $show_left_sidebar ) get_sidebar( 'right' );
 }
 
 do_action('after_layers_builder_widgets');

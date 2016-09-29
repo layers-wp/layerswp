b4f067b8e5afef702cc51663dfdca161c8603ded
Marc Perel <marc@Marcs-iMac.local>
2016-09-29 16:51:59 +0200

    Removed URL input type, not viable

--- core/helpers/forms.php
+++ core/helpers/forms.php
@@ -160,12 +160,6 @@ class Layers_Form_Elements {
 				<input type="text" <?php echo implode ( ' ' , $input_props ); ?> value="<?php echo esc_attr( $input->value ); ?>" />
 			<?php break;
 			/**
-			* URL Inputs
-			*/
-			case 'url' : ?>
-				<input type="url" <?php echo implode ( ' ' , $input_props ); ?> value="<?php echo esc_attr( $input->value ); ?>" />
-			<?php break;
-			/**
 			* Number Inputs
 			*/
 			case 'number' :

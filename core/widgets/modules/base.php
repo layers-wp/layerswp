826b260b4a757e719f32e7f4ffc00a2399476249
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-04-17 16:16:35 +0530

    Base animation to include translucent effect for 0.3 opacity

--- core/widgets/modules/base.php
+++ core/widgets/modules/base.php
@@ -19,7 +19,7 @@ if( !class_exists( 'Layers_Widget' ) ) {
 
 		public $inline_css;
 
-        public $animation_class = 'x-fade-in-up delay-200';
+        public $animation_class = 'x-fade-in-up';
 
 		//  Defaults
 
@@ -57,7 +57,7 @@ if( !class_exists( 'Layers_Widget' ) ) {
                 'on' === $this->check_and_return( $instance , 'design', 'advanced', 'animation' ) &&
                 strlen($this->animation_class) !== 0
             ) {
-		       return 'do-animate animated-1s ' . $this->animation_class;
+		       return 'do-animate delay-200 translucent animated-1s ' . $this->animation_class;
             }
             return '';
         }
